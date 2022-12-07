cae6877b822f416bf622bfd06d05d6981437f242
Jerrykebede <eyerusalem_ke@yahoo.com>
2022-12-07 10:52:47 -0800

    task 2

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""a function that returns a set of common
+elements in two sets"""
+
+
+def common_elements(set_1, set_2):
+    for set_1 & set_2:
+        c_set = common_elements
+        return c_set
+    return common_elements
