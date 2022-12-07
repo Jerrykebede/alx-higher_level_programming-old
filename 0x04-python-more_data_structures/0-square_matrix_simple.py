c91a063b89e1ee3fd11270415843ddecc66bdf1f
Jerrykebede <eyerusalem_ke@yahoo.com>
2022-12-07 11:46:15 -0800

    task 0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,12 +1,7 @@
 #!/usr/bin/python3
-
-""" a function that computes the
-square value of all integers of a matrix"""
-
-
 def square_matrix_simple(matrix=[]):
-    new_mat = [x[:] for x in matrix]
-    for i in range(len(new_mat)):
-        for j in range(len(new_mat[i])):
-            new_mat[i][j] *= new_mat[i][j]
-            return new_mat
+    res = []
+    for item in matrix:
+        item = list(map(lambda x: x*x, item))
+        res.append(item)
+         return res
