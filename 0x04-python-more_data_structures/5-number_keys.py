c848be826aa0f5537a8dc04284d4412f1367bb01
Jerrykebede <eyerusalem_ke@yahoo.com>
2022-12-15 06:30:06 -0800

    task 5

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -1,15 +1,3 @@
 #!/usr/bin/python3
-
-"""a function that returns
-the number of keys in a dictionary"""
-
-
-def number_keys(a_dictionary):
-    keys = 0
-    for key in a_dictionary.keys():
-        keys += 1
-        return keys
-
-
-    """return len(a_dictionary.keys()) can be used
-    alternatively to extract the number of keys."""
+def number_keys(my_dict):
+    return len(my_dict)
