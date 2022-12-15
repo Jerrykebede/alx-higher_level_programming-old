9089b8721e6a0ad35f35f8a99b0501b08051e659
Jerrykebede <eyerusalem_ke@yahoo.com>
2022-12-15 05:57:46 -0800

    task 7

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
-"""Write a function that replaces or adds key/value in a dictionary"""
 
-def update_dictionary(a_dictionary, key, value):
-    a_dictionary[key] = value
-    return a_dictionary
+def simple_delete(a_dictionary, key=""):
+    if key in a_dictionary:
+        del a_dictionary[key]
+        return (a_dictionary)
