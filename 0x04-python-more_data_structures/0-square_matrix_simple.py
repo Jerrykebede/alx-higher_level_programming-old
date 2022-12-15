fbe12fb6aa8bb06291e56bbe4a2cf64be60ecc68
Jerrykebede <eyerusalem_ke@yahoo.com>
2022-12-15 02:37:57 -0800

    task 0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    res = []
-    for item in matrix:
-        item = list(map(lambda x: x*x, item))
-        res.append(item)
-         return res
+    new_matrix = [[x ** 2 for x in row] for row in matrix]
+    return new_matrix
