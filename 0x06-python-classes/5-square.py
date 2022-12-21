66b47828b463b7eabe8ca176e21d763e488bd0fb
Jerrykebede <eyerusalem_ke@yahoo.com>
2022-12-21 12:13:48 -0800

    task 5

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,55 +1,59 @@
 #!/usr/bin/python3
-""" class Square that defines a square"""
+"""Defines a class Square"""
 
 
 class Square:
-    """ class Square that defines a square"""
-    def __init__(self, size=0):
-        """ init square
+    """Represents a square
+    Attributes:
+     __size (int): size of a side of the square
+     """
+     def __init__(self, size=0):
+         """initializes the square
+         Args:
+         size (int): size of a side of the square
+         Returns:
+         None
+         """
+         self.size = size
 
-        Args:
-            value (int): size of the square.
-        """
-        self.size = size
+         def area(self):
+             """calculates the square's area
+             Returns:
+             The area of the square
+             """
+             return (self.__size) ** 2
 
-    @property
-    def size(self):
-        """int: private size.
+         @property
+         def size(self):
+             """getter of __size
+             Returns:
+             The size of the square
+             """
+             return self.__size
 
-        Returns:
-            Private size.
-        """
-        return self.__size
+         @size.setter
+         def size(self, value):
+             """setter of __size
+             Args:
+             value (int): size of a side of the square
+             Returns:
+             None
+             """
+             if type(value) is not int:
+                 raise TypeError("size must be an integer")
+             else:
+                 if value < 0:
+                     raise ValueError("size must be >= 0")
+                 else:
+                     self.__size = value
 
-    @size.setter
-    def size(self, value):
-        """Sets value into size, must be int.
-
-        Args:
-            value (int): size of the square.
-        """
-        if type(value) is not int:
-            raise TypeError('size must be an integer')
-        elif value < 0:
-            raise ValueError('size must be >= 0')
-        else:
-            self.__size = value  #: size of the square
-
-        def area(self):
-            """returns the area
-
-            Returns:
-                area.
-            """
-            return self.__size**2
-
-        def my_print(self):
-            """prints in stdout the square with the character #"""
-
-            if self.__size != 0:
-                for i in range(self.__size):
-                    for j in range(self.__size):
-                        print('#', end='')
-                    print()
-                else:
-                    print()
+                     def my_print(self):
+                         """prints the square
+                         Returns:
+                         None
+                         """
+                         if self.__size == 0:
+                             print()
+                             return
+                         for i in range(self.__size):
+                             print("".join(["#" for j in range(self.__size)]))
