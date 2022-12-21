db536a2192717039cb00726006c3a209c76eac21
Jerrykebede <eyerusalem_ke@yahoo.com>
2022-12-21 06:40:42 -0800

    task 3

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,26 +1,33 @@
 #!/usr/bin/python3
+"""A class Square that defines a square by:
+    (based on 2-square.py)
 
-"""a class Square that defines a
-square by: (based on 2-square.py)"""
+    """
 
 
-class Square:
-    """Instantiation with optional parameter
-    size: def __init__(self, size=0):
-size must be an integer, otherwise raise a TypeError
-exception with the message size must be an integer
-if size is less than 0, raise a ValueError exception
-with the message size must be >= 0
-Includes a Public instance method:
-def area(self): that returns the current square area"""
+    class Square:
+        """Square class with a private attribute -
+        size.
 
-    def __init__(self, size=0):
-        if type(size) is int:
-            if size < 0:
-                raise ValueError("size must be >= 0")
+        """
+
+
+        def __init__(self, size=0):
+            """Initializes the size variable as a private
+            instance artribute
+
+
+            """
             self.__size = size
-        else:
-            raise TypeError("size must be an integer")
 
-        def area(self):
-            return self.__size * self.__size
+            if not isinstance(size, int):
+                raise TypeError("size must be an integer")
+            elif size < 0:
+                raise ValueError("size must be >= 0")
+
+            def area(self):
+                """Returns the current
+                square area
+
+                """
+                return (self.__size ** 2)
