274140b64cd866fe079ae8c9e242bb4cca89b1bd
Jerrykebede <eyerusalem_ke@yahoo.com>
2022-12-21 11:59:16 -0800

    task 3

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -5,7 +5,7 @@
 class Square:
     """Represents a square
     Attributes:
-    __size (int): size of a side of the square
+        __size (int): size of a side of the square
     """
 def __init__(self, size=0):
     """initializes the square
