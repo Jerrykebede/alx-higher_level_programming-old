958dced7cdefda9ef2cec27f7ffd32583f56e117
Jerrykebede <eyerusalem_ke@yahoo.com>
2022-12-21 07:00:41 -0800

    task 5

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -12,44 +12,44 @@ class Square:
         """
         self.size = size
 
-        @property
-        def size(self):
-            """int: private size.
+    @property
+    def size(self):
+        """int: private size.
+
+        Returns:
+            Private size.
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Sets value into size, must be int.
+
+        Args:
+            value (int): size of the square.
+        """
+        if type(value) is not int:
+            raise TypeError('size must be an integer')
+        elif value < 0:
+            raise ValueError('size must be >= 0')
+        else:
+            self.__size = value  #: size of the square
+
+        def area(self):
+            """returns the area
 
             Returns:
-                Private size.
+                area.
             """
-            return self.__size
+            return self.__size**2
 
-        @size.setter
-        def size(self, value):
-            """Sets value into size, must be int.
+        def my_print(self):
+            """prints in stdout the square with the character #"""
 
-            Args:
-                value (int): size of the square.
-            """
-            if type(value) is not int:
-                raise TypeError('size must be an integer')
-            elif value < 0:
-                raise ValueError('size must be >= 0')
-            else:
-                self.__size = value  #: size of the square
-
-            def area(self):
-                """returns the area
-
-                Returns:
-                    area.
-                """
-                return self.__size**2
-
-            def my_print(self):
-                """prints in stdout the square with the character #"""
-
-                if self.__size != 0:
-                    for i in range(self.__size):
-                        for j in range(self.__size):
-                            print('#', end='')
-                        print()
-                    else:
-                        print()
+            if self.__size != 0:
+                for i in range(self.__size):
+                    for j in range(self.__size):
+                        print('#', end='')
+                    print()
+                else:
+                    print()
