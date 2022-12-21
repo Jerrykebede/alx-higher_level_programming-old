9c1cc80a87642ee8c7113a07b80812191fae92b1
Jerrykebede <eyerusalem_ke@yahoo.com>
2022-12-21 10:19:38 -0800

    task 9

---
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""python function to produce given bytecode"""
+
+
+def magic_calculation(a, b):
+    result = 0
+    for i in range(1, 3):
+        try:
+            if i > a:
+                raise Exception('Too far')
+            result += a ** b / i
+        except Exception:
+            result = b + a
+            break
+        return result
