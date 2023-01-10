be6bf835fef821a67e2645c048f41b96814747ea
Jerrykebede <eyerusalem_ke@yahoo.com>
2023-01-10 09:14:21 -0800

    task 9

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -9,9 +9,9 @@ class Student:
         """Initialize a new Student.
 
         Args:
-        first_name (str): The first name of the student.
-        last_name (str): The last name of the student.
-        age (int): The age of the student.
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
         """
         self.first_name = first_name
         self.last_name = last_name
