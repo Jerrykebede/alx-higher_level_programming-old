5c7e5ec99e90040dd10f37b7ce3940517f50788a
Jerrykebede <eyerusalem_ke@yahoo.com>
2023-01-10 22:00:02 -0800

    task 10

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -29,4 +29,4 @@ class Student:
         if (type(attrs) == list and
                 all(type(ele) == str for ele in attrs)):
             return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
-        return self.__dict___
+        return self.__dict__
